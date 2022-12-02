f1555bdb45058c9bf883754bdc5ce59f7bb08b3c
auroraptor <interestingbutno@yandex.ru>
2022-12-02 14:57:21 +0300

    feat: 👉 Main

--- src/components/AboutProject/AboutProject.jsx
+++ src/components/AboutProject/AboutProject.jsx
@@ -1,9 +1,11 @@
+import Main from "../Main/Main";
 import "./AboutProject.css";
 
 function AboutProject() {
   return (
     <div className="AboutProject">
-      <p>ABOUT PROGECT 🔜</p>
+      <p>ABOUT</p>
+      <Main />
     </div>
   );
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,20 @@
+import AboutMe from '../AboutMe/AboutMe';
+import AboutProject from '../AboutProject/AboutProject';
+import NavTab from '../NavTab/NavTab';
+import Portfolio from '../Portfolio/Portfolio';
+import Promo from '../Promo/Promo';
+import Techs from '../Techs/Techs';
 import './Main.css';
 
 function Main() {
   return (
     <div className="Main">
-
+        <Promo></Promo>
+        <NavTab></NavTab>
+        <AboutProject></AboutProject>
+        <Techs></Techs>
+        <AboutMe></AboutMe>
+        <Portfolio></Portfolio>
     </div>
   );
 }
