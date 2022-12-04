949955148d71192f828891d992407c1f7ede7625
auroraptor <interestingbutno@yandex.ru>
2022-12-04 12:40:18 +0300

    fix: title

--- src/components/Techs/Techs.jsx
+++ src/components/Techs/Techs.jsx
@@ -3,7 +3,7 @@ import "./Techs.css";
 function Techs() {
   return (
     <div className="techs">
-      <h2 className="techs__title">Технологии</h2>
+      <h3 className="techs__title">Технологии</h3>
       <p className="techs__description">7 технологий</p>
       <p className="techs__text">
         На курсе веб-разработки мы освоили технологии, которые применили в
