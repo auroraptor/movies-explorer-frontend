bca23c73c4a9e46c8c6b309c0c5e7b8f123a297b
auroraptor <interestingbutno@yandex.ru>
2022-12-06 16:11:01 +0300

    feat: 👤 profile

---
+++ src/components/HamburgerMenu/HamburgerMenu.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./HamburgerMenu.css";
+
+function HamburgerMenu() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => {
+    setClick(!click);
+  };
+  return (
+
+     <div
+      className={click ? "hamburger__icon hamburger__icon__active" : "hamburger__icon"}
+      onClick={handleClick}
+    >
+      <div className="hamburger__button hamburger__button_close" aria-label="Показать меню"/>
+    </div>
+
+    
+   
+  );
+}
+
+export default HamburgerMenu;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,38 @@
+import { Link } from "react-router-dom";
+import Logo from "../Logo/Logo";
+import HamburgerMenu from "../HamburgerMenu/HamburgerMenu";
 import "./Profile.css";
 
-function Profile() {
+function Profile({ name = "useR", email= "love@eda.jp" }) {
+  const firstName = name[0].toUpperCase() + name.slice(1).toLowerCase();
+
   return (
-    <div className="Profile">
-      <p>PROFILE 🔜</p>
+    <div className="profile">
+      <header className="profile__header">
+        <Logo></Logo>
+        <HamburgerMenu></HamburgerMenu>
+      </header>
+      <div className="profile__container">
+        <p className="profile__hello-name">{`Привет, ${firstName}!`}</p>
+        <div className="profile__user-info">
+          <div className="profile__user-info_name">
+            <p className="profile__user-info_text">Имя</p>
+            <p className="profile__user-info_text">{firstName}</p>
+          </div>
+          <div className="profile__user-info_email">
+            <p className="profile__user-info_text">E-mail</p>
+            <p className="profile__user-info_text">{email}</p>
+          </div>
+        </div>
+      </div>
+      <div className="profile__control">
+        <button className="profile__button_edit" type="button">
+        Редактировать
+        </button>
+          <Link to="/signout" className="profile__link">
+          Выйти из аккаунта
+          </Link>
+      </div>
     </div>
   );
 }
