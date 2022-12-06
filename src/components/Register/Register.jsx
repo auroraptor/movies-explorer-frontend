8c00dff38b0ff2ae80ca735acc23326141508f02
auroraptor <interestingbutno@yandex.ru>
2022-12-06 12:13:06 +0300

    feat: 📝 register

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,13 +1,18 @@
+import { Link } from "react-router-dom";
 import Input from "../Input/Input";
 import "./Register.css";
 
 function Register() {
   return (
-    <div className="register">
+    <form className="register" name="register" id="register">
       <Input label="Имя" type="text" errorText={'This is help text'}></Input>
       <Input label="E-mail" type="email" errorText={'This is help text'}></Input>
       <Input label="Пароль" type="password" errorText={'This is help text'} isVisible={true}></Input>
-    </div>
+      <div className="register__container">
+        <button className="register__button" type="submit">Зарегистрироваться</button>
+        <label className="register__label">Уже зарегистрированы? <Link to="/" className="register__link">Войти</Link></label>
+      </div>
+    </form>
   );
 }
 
