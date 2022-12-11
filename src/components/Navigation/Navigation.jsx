32a287e071e4bad34bca0586dddef278f7debd2e
auroraptor <interestingbutno@yandex.ru>
2022-12-11 16:43:26 +0300

    feat: 🇳 navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,25 +1,27 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./Navigation.css";
 
 function Navigation() {
   return (
-    <nav className="navigation">
-      <div className="navigation__container">
-        {" "}
-        <Link to="/" className="navigation__link">
+    <nav className="menu">
+      <div className="menu__container">
+        <NavLink to="/" activeClassName="menu__link_active" className="menu__link menu__link_main">
           Главная
-        </Link>
-        <Link to="/movies" className="navigation__link">
+        </NavLink>
+        <NavLink to="/movies" activeClassName="menu__link_active" className="menu__link menu__link_active">
           Фильмы
-        </Link>
-        <Link to="/saved-movies" className="navigation__link">
+        </NavLink>
+        <NavLink to="/saved-movies"  activeClassName="menu__link_active" className="menu__link">
           Сохранённые фильмы
-        </Link>
+        </NavLink>
       </div>
 
-      <Link to="/profile" className="navigation__link navigation__profile">
-        Аккаунт
-      </Link>
+      <NavLink to="/profile" className="menu__link menu__profile">
+       <div className="menu__profile_container">
+        <div className="menu__profile_type_icon"></div>
+        <p className="menu__profile_type_text">Аккаунт</p>
+       </div>
+      </NavLink>
     </nav>
   );
 }
--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -3,7 +3,7 @@ import './SearchForm.css';
 
 function SearchForm() {
   return (
-    <form name="search" id="search" className="search-form">
+    <form name="search" id="search" className="search-form" onSubmit={() => alert('not really submit')}>
       <div className="search-form__container">
         <input type="search" className="search-form__input" id="site-search" name="search" placeholder="Фильм"></input>
         <button type="submit" className="search-form__button" aria-label="Найти">
