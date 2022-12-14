d3214ac84c29bb4415ceadc0b0367556af8f8482
auroraptor <interestingbutno@yandex.ru>
2022-12-14 23:18:45 +0300

    feat:📱 responsive promo

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,30 +1,34 @@
-import { Link } from 'react-router-dom';
-import AboutMe from '../AboutMe/AboutMe';
-import AboutProject from '../AboutProject/AboutProject';
-import Header from '../Header/Header';
-import NavTab from '../NavTab/NavTab';
-import Portfolio from '../Portfolio/Portfolio';
-import Promo from '../Promo/Promo';
-import Techs from '../Techs/Techs';
-import Footer from '../Footer/Footer';
-import './Main.css';
+import { Link } from "react-router-dom";
+import AboutMe from "../AboutMe/AboutMe";
+import AboutProject from "../AboutProject/AboutProject";
+import Header from "../Header/Header";
+import NavTab from "../NavTab/NavTab";
+import Portfolio from "../Portfolio/Portfolio";
+import Promo from "../Promo/Promo";
+import Techs from "../Techs/Techs";
+import Footer from "../Footer/Footer";
+import "./Main.css";
 
 function Main() {
   return (
     <div className="main">
-        <Header className={"header header_place_main"}>
-        <div className="main__control">
-        <Link to="/signup" className="main__button main__button_transparent">Регистрация</Link>
-        <Link to="/signin" className="main__button main__button_green">Войти</Link>
-      </div>
-        </Header>
-        <Promo></Promo>
-        <NavTab></NavTab>
-        <AboutProject></AboutProject>
-        <Techs></Techs>
-        <AboutMe></AboutMe>
-        <Portfolio></Portfolio>
-        <Footer></Footer>
+      <Header className={"header header_place_main"}>
+        <div className="header__navigation_page_main">
+          <Link to="/signup" className="header__button header__button_color_transparent">
+            Регистрация
+          </Link>
+          <Link to="/signin" className="header__button header__button_color_green">
+            Войти
+          </Link>
+        </div>
+      </Header>
+      <Promo></Promo>
+      <NavTab></NavTab>
+      <AboutProject></AboutProject>
+      <Techs></Techs>
+      <AboutMe></AboutMe>
+      <Portfolio></Portfolio>
+      <Footer></Footer>
     </div>
   );
 }
