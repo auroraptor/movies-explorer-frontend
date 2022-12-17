e76e7767d8486902bca98a6af2108392cc40c1a9
auroraptor <interestingbutno@yandex.ru>
2022-12-17 15:00:28 +0300

    feat: 👨🏻‍💻 responsive

--- src/components/Techs/Techs.jsx
+++ src/components/Techs/Techs.jsx
@@ -4,34 +4,34 @@ function Techs() {
   return (
     <article className="techs" id="techs">
       <h3 className="techs__title">Технологии</h3>
-      <p className="techs__description">7 технологий</p>
-      <p className="techs__text">
+      <p className="techs__subtitle">7 технологий</p>
+      <p className="techs__description">
         На курсе веб-разработки мы освоили технологии, которые применили в
         дипломном проекте.
       </p>
-      <div className="techs__container">
-        <div className="techs__item">
-          <p className="techs__content">HTML</p>
-        </div>
-        <div className="techs__item">
-          <p className="techs__content">CSS</p>
-        </div>
-        <div className="techs__item">
-          <p className="techs__content">JS</p>
-        </div>
-        <div className="techs__item">
-          <p className="techs__content">React</p>
-        </div>
-        <div className="techs__item">
-          <p className="techs__content">Git</p>
-        </div>
-        <div className="techs__item">
-          <p className="techs__content">Express.js</p>
-        </div>
-        <div className="techs__item">
-          <p className="techs__content">mongoDB</p>
-        </div>
-      </div>
+      <ul className="techs__list">
+        <li className="techs__item">
+          <span className="techs__content">HTML</span>
+        </li>
+        <li className="techs__item">
+          <span className="techs__content">CSS</span>
+        </li>
+        <li className="techs__item">
+          <span className="techs__content">JS</span>
+        </li>
+        <li className="techs__item">
+          <span className="techs__content">React</span>
+        </li>
+        <li className="techs__item">
+          <span className="techs__content">Git</span>
+        </li>
+        <li className="techs__item">
+          <span className="techs__content">Exress.js</span>
+        </li>
+        <li className="techs__item">
+          <span className="techs__content">mongoDB</span>
+        </li>
+      </ul>
     </article>
   );
 }
