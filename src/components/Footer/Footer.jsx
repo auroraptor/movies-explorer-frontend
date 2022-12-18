575760a0eef268698e0d65cd15f4e87a3ac959bb
auroraptor <interestingbutno@yandex.ru>
2022-12-18 14:35:52 +0300

    feat: 🙌 responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,26 @@
-import './Footer.css';
+import "./Footer.css";
 
 function Footer() {
   return (
     <div className="footer">
-      <h5 className="footer__collab">Учебный проект Яндекс.Практикум х BeatFilm.</h5>
-      <a href="https://practicum.yandex.ru/" className="footer__link footer__link_practicum">Яндекс.Практикум</a>
-      <a href="https://github.com/auroraptor/movies-explorer-frontend/" className="footer__link footer__link_github">Github</a>
-      <p className="footer__copyright">©2020</p>
+      <h5 className="footer__collab">
+        Учебный проект Яндекс.Практикум х BeatFilm.
+      </h5>
+      <div className="footer__container">
+        <a
+          href="https://practicum.yandex.ru/"
+          className="footer__link footer__link_practicum"
+        >
+          Яндекс.Практикум
+        </a>
+        <a
+          href="https://github.com/auroraptor/movies-explorer-frontend/"
+          className="footer__link footer__link_github"
+        >
+          Github
+        </a>
+        <p className="footer__copyright">©2020</p>
+      </div>
     </div>
   );
 }
