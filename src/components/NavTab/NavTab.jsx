9c9e2db55ce53f40cbfbf45e9c76c029cb5414f0
auroraptor <interestingbutno@yandex.ru>
2022-12-18 11:44:58 +0300

    fix: thead

--- src/components/NavTab/NavTab.jsx
+++ src/components/NavTab/NavTab.jsx
@@ -1,26 +1,27 @@
-import { Link } from "react-router-dom";
 import "./NavTab.css";
 
 function NavTab() {
   return (
     <table className="navtab">
-      <tr className="navtab__row">
-        <th>
-          <a href="/#about-project" className="navtab__link">
-            О проекте
-          </a>
-        </th>
-        <th>
-          <a href="/#techs" className="navtab__link">
-            Технологии
-          </a>
-        </th>
-        <th>
-          <a href="/#about-me" className="navtab__link">
-            Студент
-          </a>
-        </th>
-      </tr>
+      <thead>
+        <tr className="navtab__row">
+          <th>
+            <a href="/#about-project" className="navtab__link">
+              О проекте
+            </a>
+          </th>
+          <th>
+            <a href="/#techs" className="navtab__link">
+              Технологии
+            </a>
+          </th>
+          <th>
+            <a href="/#about-me" className="navtab__link">
+              Студент
+            </a>
+          </th>
+        </tr>
+      </thead>
     </table>
   );
 }
