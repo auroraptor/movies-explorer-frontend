864ba4857594bdce69b3baac0ab0a8e8fad78f5c
auroraptor <interestingbutno@yandex.ru>
2022-12-22 19:46:39 +0300

    fix: 🔍 search form

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -5,7 +5,7 @@ function SearchForm() {
   return (
     <form name="search" id="search" className="search-form" onSubmit={() => alert('not really submit')}>
       <div className="search-form__container">
-        <input type="search" className="search-form__input" id="site-search" name="search" placeholder="Фильм"></input>
+        <input type="search" className="search-form__input" id="site-search" name="search" placeholder="Фильм" required></input>
         <button type="submit" className="search-form__button search-form__icon" aria-label="Найти">
         </button>
       </div>
