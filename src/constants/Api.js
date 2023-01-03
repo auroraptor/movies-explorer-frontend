9835131a35f8fff9213bb97be23795897db324c5
auroraptor <interestingbutno@yandex.ru>
2023-01-03 13:45:54 +0300

    fix: 🎬 url

--- src/constants/Api.js
+++ src/constants/Api.js
@@ -1,2 +1,2 @@
 export const MAIN_API = "https://hey.nomoredomains.club/api/";
-export const MOVIES_API = "https://api.nomoreparties.co/";
+export const MOVIES_API = "https://api.nomoreparties.co/beatfilm-movies";
