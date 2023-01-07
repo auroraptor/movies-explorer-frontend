97bb7300bd2eeb38ed3d34b11e74025afd5dcc25
auroraptor <interestingbutno@yandex.ru>
2023-01-07 17:22:25 +0300

    fix: 😬 name regex

--- src/constants/regex.js
+++ src/constants/regex.js
@@ -1,2 +1,2 @@
 export const VALID_EMAIL_REGEX = /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
-export const VALID_NAME_REGEX = /[A-Za-z_А-Яа-я\-Ёё]/g;
+export const VALID_NAME_REGEX = /^[A-Za-z_А-Яа-я\-Ёё]+$/;
